9ee3bc00202e3a709e5d7aed750e092794284bf3
Donaldson17 <donaldsonokozi@gmail.com>
2025-07-13 11:03:23 -0700

    Refactor tests and components: update App to render static content, modify ExampleComponent and TestComponent for consistency

--- src/components/ExampleComponent.js
+++ src/components/ExampleComponent.js
@@ -1,14 +1,10 @@
 import React from "react";
 
-// no need to modify anything in here. The second test is looking for the returned JSX from this component
 function ExampleComponent() {
   return (
-    <div className="example">
-      <img
-        src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif"
-        alt="a deeply impressed man, perhaps thinking 'whoa'"
-      />
-      <p>Whoa!</p>
+    <div>
+      <img src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" alt="Sunglasses" />
+      <p>Sunglasses GIF</p>
     </div>
   );
 }
